cc982a2e9f2fd6a6664632986390c72a49531d4e
Rakshith Acharya <rakshixh@gmail.com>
2024-07-25 23:52:42 +0530

    updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Turn as Hamburger } from "hamburger-react";
+import { Cross as Hamburger } from "hamburger-react";
 import HeaderCSS from "../css/Header.module.css";
 import { linksData, title } from "../linksData";
-import Loader from "../components/Loader";
 
 function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
   const [pages, setPages] = useState([]);
-  const [showLoader, setShowLoader] = useState(false);
 
   useEffect(() => {
     setPages(Object.keys(linksData));
@@ -17,28 +15,17 @@ function Header() {
   const handleLinkClick = (page) => {
     setMenuOpen(false);
     localStorage.setItem("currentpage", page);
-
-    // Show the loader and refresh the window
-    setShowLoader(true);
-    setTimeout(() => {
-      window.location.reload();
-    }, 500);
+    window.dispatchEvent(new Event("storage"));
   };
 
   const handleTitleClick = () => {
     const firstPageKey = Object.keys(linksData)[0];
     localStorage.setItem("currentpage", firstPageKey);
-
-    // Show the loader and refresh the window
-    setShowLoader(true);
-    setTimeout(() => {
-      window.location.reload();
-    }, 500);
+    window.dispatchEvent(new Event("storage"));
   };
 
   return (
     <div className={HeaderCSS.container}>
-      {showLoader && <Loader />}
       <Link
         to="/"
         style={{ textDecoration: "none", color: "white" }}
