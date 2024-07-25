66cd06c008b0ee5e008cb7cfdf3b8acf9c7568a8
Rakshith Acharya <rakshixh@gmail.com>
2024-07-25 23:11:33 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import { linksData } from "./linksData";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          {Object.keys(linksData).map((pageKey, index) => (
+            <Route
+              key={index}
+              path={index === 0 ? "/" : `/${pageKey}`}
+              element={<Home />}
+            />
+          ))}
         </Routes>
       </Router>
     </div>
