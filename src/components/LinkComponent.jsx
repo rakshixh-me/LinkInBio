043431fd3455c1f64fd28ecc4e65a4ab927af259
Rakshith Acharya <rakshixh@gmail.com>
2024-08-28 23:54:30 +0530

    Updated Link Component

--- src/components/LinkComponent.jsx
+++ src/components/LinkComponent.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import styles from "../css/LinkComponent.module.css";
+import LinkStyle from "../css/LinkComponent.module.css";
+// import LightArrow from "../assets/LightArrow.svg";
+import DarkArrow from "../assets/DarkArrow.svg";
 
-const LinkComponent = ({ linktitle, url }) => {
+function LinkComponent() {
   return (
-    <div className={styles.container}>
-      <a href={url} className={styles.link}>
-        {linktitle}
-      </a>
+    <div>
+      <button className={LinkStyle.btn}>
+        LINK
+        <img src={DarkArrow} alt="Arrow" className={LinkStyle.arrowIcon} />
+      </button>
     </div>
   );
-};
+}
 
 export default LinkComponent;
