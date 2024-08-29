e86a27ce275c62e99f700d43d5f9a1935ff89f80
Rakshith Acharya <rakshixh@gmail.com>
2024-08-29 13:47:46 +0530

    Updated linksData.js

--- src/linksData.js
+++ src/linksData.js
@@ -1,6 +1,47 @@
+import { resolveProfileImagePath } from "./utility/ResolveProfileImagePath";
+
+// Customizable data, provide hex codes of the colors
+export const styleData = {
+  AppBackgroundColor: "",
+  OutlineButtonStyle: {
+    buttonBackgroundColor: "",
+    buttonTextColor: "",
+    buttonBorderColor: "",
+  },
+  FilledButtonStyle: {
+    buttonBackgroundColor: "",
+    buttonTextColor: "",
+  },
+};
+
+// Can be a URL, a full file name with extension, or just a file name without extension
+const profileImagePath = "";
+
 export const linksData = {
   logo: "Lnk",
-  profileImage: "profile.jpg",
-  name: "John Doe",
+  profileImage: resolveProfileImagePath(profileImagePath),
+  name: "Lina Doe",
   designation: "Designer & Web Developer",
+  links: [
+    {
+      linkText: "Home",
+      linkUrl: "#",
+      linkBtn: "Outline",
+    },
+    {
+      linkText: "About",
+      linkUrl: "#",
+      linkBtn: "Outline",
+    },
+    {
+      linkText: "Services",
+      linkUrl: "#",
+      linkBtn: "Outline",
+    },
+    {
+      linkText: "Contact",
+      linkUrl: "#",
+      linkBtn: "Filled",
+    },
+  ],
 };
