312f6d9e08a4050d1248d185766dca796c52dcdf
Rakshith Acharya <rakshixh@gmail.com>
2024-08-29 13:50:22 +0530

    Updated LinkComponent and its style

--- src/components/LinkComponent.jsx
+++ src/components/LinkComponent.jsx
@@ -1,15 +1,50 @@
 import React from "react";
 import LinkStyle from "../css/LinkComponent.module.css";
-// import LightArrow from "../assets/LightArrow.svg";
+import { styleData } from "../linksData";
+import LightArrow from "../assets/LightArrow.svg";
 import DarkArrow from "../assets/DarkArrow.svg";
 
-function LinkComponent() {
+function LinkComponent({
+  linkText = "LINK",
+  linkUrl = "#",
+  linkBtn = " Outline",
+}) {
   return (
     <div>
-      <button className={LinkStyle.btn}>
-        LINK
-        <img src={DarkArrow} alt="Arrow" className={LinkStyle.arrowIcon} />
-      </button>
+      {linkBtn === "Outline" ? (
+        <a href={linkUrl} className={LinkStyle.aBtn}>
+          <button
+            className={LinkStyle.btn}
+            style={{
+              backgroundColor:
+                styleData.OutlineButtonStyle.buttonBackgroundColor ||
+                "transparent",
+              color: styleData.OutlineButtonStyle.buttonTextColor || "#0A0A0A",
+              border: `1px solid ${
+                styleData.OutlineButtonStyle.buttonBorderColor || "#0A0A0A"
+              }`,
+            }}
+          >
+            {linkText}
+            <img src={DarkArrow} alt="Arrow" className={LinkStyle.arrowIcon} />
+          </button>
+        </a>
+      ) : (
+        <a href={linkUrl} className={LinkStyle.aBtn}>
+          <button
+            className={LinkStyle.btn}
+            style={{
+              backgroundColor:
+                styleData.FilledButtonStyle.buttonBackgroundColor || "#0A0A0A",
+              color: styleData.FilledButtonStyle.buttonTextColor || "#EBEBE3",
+              border: "none",
+            }}
+          >
+            {linkText}
+            <img src={LightArrow} alt="Arrow" className={LinkStyle.arrowIcon} />
+          </button>
+        </a>
+      )}
     </div>
   );
 }
