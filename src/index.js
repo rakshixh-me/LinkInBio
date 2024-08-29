f662b916104461be6ca98f6fba8e65f5acf3e76c
Rakshith Acharya <rakshixh@gmail.com>
2024-08-29 13:46:24 +0530

    Added Theme Provider for index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import ThemeProvider from "./ThemeProvider";
+import { styleData } from "./linksData";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const theme = {
+  backgroundColor: styleData.AppBackgroundColor || "#EBEBE3",
+};
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
