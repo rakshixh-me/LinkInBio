2a252efa8a4bf52a053a2dca4aabe9dad7cd3760
Rakshith Acharya <rakshixh@gmail.com>
2024-08-29 13:49:56 +0530

    Updated Home.jsx and its Style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,32 @@
 import React from "react";
 import { linksData } from "../linksData";
-import Profile from "../assets/profile.jpg";
 import Link from "../components/LinkComponent";
+import HomeCSS from "../css/Home.module.css";
 
 function Home() {
+  const profileImageSrc = linksData.profileImage;
+  console.log(profileImageSrc);
   return (
     <div>
       <header>
         <h1>{linksData.logo}</h1>
       </header>
       <main>
-        <img src={Profile} alt="Profile" className="img" />
+        {profileImageSrc &&
+          profileImageSrc !== "Please Provide Profile Image" && (
+            <img src={profileImageSrc} alt="Profile" className="img" />
+          )}
         <h2>{linksData.name}</h2>
         <h3>{linksData.designation}</h3>
-        <div>
-          <Link />
+        <div className={HomeCSS.linkContainer}>
+          {linksData.links.map((link, index) => (
+            <Link
+              key={index}
+              linkText={link.linkText}
+              linkUrl={link.linkUrl}
+              linkBtn={link.linkBtn}
+            />
+          ))}
         </div>
       </main>
     </div>
